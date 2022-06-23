1367edde5a549ed959494d5a94617014db0a6e7f
Luis Eduardo M <luiseduardomc019@gmail.com>
2022-06-23 18:19:53 -0300

    search by department component

---
+++ src/components/Department.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Department = () => {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    <h2>Search by Department.</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Department;
